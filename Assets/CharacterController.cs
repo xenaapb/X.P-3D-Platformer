3c3fc1f7ea0f306155e28e4bb9751b14a6d651d3
xenaapb <xena.apb@gmail.com>
2022-11-17 15:30:35 +0000

    added char ctrl

--- Assets/CharacterController.cs
+++ Assets/CharacterController.cs
@@ -8,23 +8,38 @@ public class CharacterController : MonoBehaviour
 
     float camRotation = 0.0f;
     GameObject cam;
+    Rigidbody myRigidbody;
 
-    float rotationSpeed = 2.0f;
-    float camRotationSpeed = 1.5f;
+    bool isOnGround;
+    public GameObject groundChecker;
+    public LayerMask groundLayer;
+
+    public float rotationSpeed = 2.0f;
+    public float camRotationSpeed = 1.5f;
+    public float jumpForce = 300.0f;
 
 
     void Start()
     {
         cam = GameObject.Find("Main Camera");
+        myRigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
+    
+    public float maxSpeed = 1.0f;
+    public float rotation = 0.0f;
+// Update is called once per frame
 
-    float maxSpeed = 1.0f;
-    float rotation = 0.0f;
     void Update()
     {
-        transform.position = transform.position + (transform.forward * Input.GetAxis("Vertical"));
+        isOnGround = Physics.CheckSphere(groundChecker.transform.position, 0.1f, groundLayer);
+
+        if (isOnGround == true && Input.GetKeyDown(KeyCode.Space))
+      {
+          myRigidbody.AddForce(transform.up * jumpForce);
+      }
+        Vector3 newVelocity = (transform.forward * Input.GetAxis("Vertical") * maxSpeed) + (transform.right * Input.GetAxis("Horizontal") * maxSpeed);
+        myRigidbody.velocity = new Vector3(newVelocity.x, myRigidbody.velocity.y, newVelocity.z);
 
         rotation = rotation + Input.GetAxis("Mouse X") * rotationSpeed;
         transform.rotation = Quaternion.Euler(new Vector3(0.0f, rotation, 0.0f));
